c68be2be994b311d769ab999038d7a0b40b1a7c1
saisricharan <saisricharan.7@gmail.com>
2024-02-12 22:31:52 +0530

    html and css done

---
+++ script.js
@@ -0,0 +1,9 @@
+const container= document.querySelector('.container');
+const search= document.querySelector('.search-box button');
+const weatherBox= document.querySelector('.weather-box');
+const weatherDetails= document.querySelector('.weather-details');
+
+search.addEventListener('click',()=>{
+    const APIKEY='1494c8141154dc2efa8dd948498f5b54';
+    
+})
